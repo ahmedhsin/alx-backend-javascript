c69d4d6e39b0ef4646d178190f5b5e78b278a1e1
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-09-29 03:54:54 +0200

    fix task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => res);
+    .then((res) => {
+      for (let i = 0; i < res.length; i += 1) {
+        const tmp = { ...res[i] };
+        if (res[i].status === 'fulfilled') res[i] = { status: tmp.status, value: tmp.value };
+        else res[i] = { status: tmp.status, value: String(tmp.reason) };
+      }
+      console.log(res);
+      return res;
+    });
 }
