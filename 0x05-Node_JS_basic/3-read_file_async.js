c3c003177d0e046c93ba4cc8f6e1b84c48cd2a22
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-28 04:39:04 +0200

    add task 3, 4, 5

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,39 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, d) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const data = d.split('\n');
+      const departs = {};
+      let num = 0;
+      data.forEach((row) => {
+        const cols = row.split(',');
+        if (cols.length === 4) {
+          const dep = cols[3];
+          const name = cols[0];
+          if (departs[dep] === undefined) {
+            departs[dep] = [name];
+          } else {
+            departs[dep].push(name);
+          }
+          num += 1;
+        }
+      });
+      delete departs.field;
+      num -= 1;
+      console.log(`Number of students: ${num}`);
+      for (const [key, value] of Object.entries(departs)) {
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      }
+      resolve();
+    });
+  });
+  return promise;
+}
+
+module.exports = countStudents;
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,54 @@
 #!/usr/bin/node
+const http = require('http');
+const fs = require('fs');
+
+function countStudents(path, res) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, d) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const data = d.split('\n');
+      const departs = {};
+      let num = 0;
+      data.forEach((row) => {
+        const cols = row.split(',');
+        if (cols.length === 4) {
+          const dep = cols[3];
+          const name = cols[0];
+          if (departs[dep] === undefined) {
+            departs[dep] = [name];
+          } else {
+            departs[dep].push(name);
+          }
+          num += 1;
+        }
+      });
+      delete departs.field;
+      num -= 1;
+      res.write(`Number of students: ${num}\n`);
+      for (const [key, value] of Object.entries(departs)) {
+        const cont = `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+        res.write(cont);
+      }
+      resolve();
+    });
+  });
+  return promise;
+}
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2], res).then(() => {
+      res.end();
+    });
+  }
+});
+
+app.listen(1245);
+module.exports = app;
