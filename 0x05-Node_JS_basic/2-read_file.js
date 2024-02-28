fb31d232c0c32bca4dfda02c05fd78ea56cc708d
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-28 03:47:07 +0200

    add task 2 sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,35 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  let data = null;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+    data = data.split('\n');
+  } catch (e) {
+    throw Error('Cannot load the database');
+  }
+  const departs = {};
+  let num = 0;
+  data.forEach((row) => {
+    const cols = row.split(',');
+    if (cols.length === 4) {
+      const dep = cols[3];
+      const name = cols[0];
+      if (departs[dep] === undefined) {
+        departs[dep] = [name];
+      } else {
+        departs[dep].push(name);
+      }
+      num += 1;
+    }
+  });
+  delete departs.field;
+  num -= 1;
+  console.log(`Number of students: ${num}`)
+  for (const [key, value] of Object.entries(departs)) {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
